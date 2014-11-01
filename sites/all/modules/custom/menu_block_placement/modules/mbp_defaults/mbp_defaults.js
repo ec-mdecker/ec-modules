12d900d902d6417a3982afcfa971b8e26668b265
Mike Decker <pookmish@gmail.com>
2014-10-31 19:45:27 -0500

    initial commit

---
+++ sites/all/modules/custom/menu_block_placement/modules/mbp_defaults/mbp_defaults.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Configures newly created contextual links to work with quicktabs.
+ */
+
+(function ($) {
+  Drupal.behaviors.quicktabsContextual = {
+    attach: function (context, settings) {
+      $('a.quicktabs-contextual', context).once('init-quicktabs-contextual-processed').click(function () {
+        var rel = $(this).attr('rel');
+        $('#' + rel).click();
+        return false;
+      });
+    }
+  }
+})(jQuery);
